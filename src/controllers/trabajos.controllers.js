41c4a82ee488f8198d4c5551b40a51e61e35ccf4
Jonathan San Miguel <guerreroz80@outlook.es>
2023-03-06 10:31:02 -0600

    correciones en el controller update e instalacion de nodemon

--- src/controllers/trabajos.controllers.js
+++ src/controllers/trabajos.controllers.js
@@ -86,8 +86,7 @@ const CrearTrabajo = async(req, res) => {
         // res successful
         res.status(200).json({
             status: true,
-            message: 'Registro creado Satisfactoriamente',
-            trabajo
+            message: 'Registro creado Satisfactoriamente'
         })
 
         // En caso de error, res json con el error.
@@ -103,23 +102,45 @@ const CrearTrabajo = async(req, res) => {
 //Funcion para actualizar un registro.
 const ActualizarTrabajo = async(req, res) => {
     try {
-        // Busca en la BD el registro a actualizar.
         const registroAnterior = await Trabajo.findById(req.params.id)
+        console.log(registroAnterior);
 
-        // Con el resultado de la busqueda anterior, borra las imagenes en cloudinary.
-        if(!registroAnterior) {
-            // Si el registro no existe, termina el proceso. 
-            return res.status(404).json({
-                status: false,
-                message: 'El Registro no Existe'
-            })
-        } else {
-            await DeleteImage(registroAnterior.image.public_id)
-            await DeleteImage(registroAnterior.factura.public_id)
+        if(req.files?.image) {
+            // Busca en la BD el registro a actualizar.
+            console.log('si hay imagen para borrar');
+    
+            // Con el resultado de la busqueda anterior, borra las imagenes en cloudinary.
+            if(!registroAnterior) {
+                // Si el registro no existe, termina el proceso. 
+                return res.status(404).json({
+                    status: false,
+                    message: 'El Registro no Existe'
+                })
+            } else {
+                await DeleteImage(registroAnterior.image.public_id)
+                console.log('se borro la imagen');
+            }
         }
 
+        if(req.files?.factura) {
+            // Busca en la BD el registro a actualizar.
+            console.log('si hay factura para borrar');
+    
+            // Con el resultado de la busqueda anterior, borra las imagenes en cloudinary.
+            if(!registroAnterior) {
+                // Si el registro no existe, termina el proceso. 
+                return res.status(404).json({
+                    status: false,
+                    message: 'El Registro no Existe'
+                })
+            } else {
+                await DeleteImage(registroAnterior.factura.public_id)
+                console.log('se borro la factura');
+            }
+        }
+        
         // Desestructuracion de los parametros del req.body.
-        const { nombre, descripcion, fecha  } = req.body
+        const { nombre, descripcion, fecha } = req.body
 
         // Creacion de una instancia del modelo con los parametros.
         const trabajo = new Trabajo({ nombre, descripcion, fecha })
@@ -127,22 +148,44 @@ const ActualizarTrabajo = async(req, res) => {
         // Carga la imagen del form.
         if(req.files?.image) {
             const result = await UploadImage(req.files.image.tempFilePath)
+
+            console.log('si hay imagen para subir');
+
             trabajo.image = {
                 public_id: result.public_id,
                 secure_url: result.secure_url
             }
             await fs.unlink(req.files.image.tempFilePath)
+        } else {
+            console.log('no se cambia la imagen');
+            trabajo.image = {
+                public_id: registroAnterior.image.public_id,
+                secure_url: registroAnterior.image.secure_url
+            }
         }
 
         // Carga la factura en PDF del form.
         if(req.files?.factura) {
             const result = await UploadImage(req.files.factura.tempFilePath)
+
+            console.log('si hay factura para subir');
+
             trabajo.factura = {
                 public_id: result.public_id,
                 secure_url: result.secure_url
             }
             await fs.unlink(req.files.factura.tempFilePath)
+        } else {
+            console.log('no se cambia la factura');
+            trabajo.factura = {
+                public_id: registroAnterior.factura.public_id,
+                secure_url: registroAnterior.factura.secure_url
+            }
         }
+
+
+        console.log(trabajo)
+
 
         // Actualiza el registro mandandole un objeto con los datos.
         await Trabajo.findByIdAndUpdate(req.params.id, {
